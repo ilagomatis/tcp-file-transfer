3da19b5ddf81221f5c7153f6265f78d6f6e2a4d4
ilias <hliaslago@gmail.com>
2023-08-29 10:15:40 +0300

    modify client main routine

--- client.c
+++ client.c
@@ -2,6 +2,12 @@
 #include "packet/packet.h"
 #include "string_utils/string_utils.h"
 #include "tcp_client_connect/tcp_client_connect.h"
+
+#define PORT 8080
+#define FILENAME "../string/cat.rgb"
+#define CHUNK 500
+#define IP "10.64.83.216"
+
 int main(){
-    sendfile("asd", 1000, "../string/cat.rgb", 1000);
+    sendfile(IP, PORT, FILENAME, CHUNK);
 }
