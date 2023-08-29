1fa6cf021b73d9bfeadb3aa72333c88274525fa3
ilias <hliaslago@gmail.com>
2023-08-29 10:39:18 +0300

    server main routine

--- server.c
+++ server.c
@@ -1,7 +1,6 @@
-#include "tcp_server_connect/tcp_server_connect.h"
+#include "serverproto/getfile.h"
 #define PORT 8080
 int main()
 {
-    int server;
-    int client = tcp_server_connect(PORT, &server);
+    getfile(PORT);
 }
