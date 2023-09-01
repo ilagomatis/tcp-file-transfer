1f0e91f92c4f38ef79e7d7ec3bfeeb59645df3d0
ilias <hliaslago@gmail.com>
2023-09-01 10:49:04 +0300

    removed prints

--- clientproto/send_file.c
+++ clientproto/send_file.c
@@ -25,18 +25,15 @@ int sendfile(char* IP, uint16_t port, char* pathtofile, int chunksize){
     
     char* metadata = joinPacketSizes(message, SEPARATOR);
     int metadatasize = strlen(metadata);
-    //printf("metadataSize: %d\n", metadatasize);
-    //printf("%s\n", metadata);
+
     char* firstmessage = (char*)malloc(FIRST_MSG_SZ * sizeof(char));
     sprintf(firstmessage, "%d", metadatasize);
 
     sock = tcp_client_connect(IP, port);
 
     send(sock, firstmessage, FIRST_MSG_SZ, 0);
-    //printf("Bytes sent: %d\n", FIRST_MSG_SZ);
 
     send(sock, metadata, metadatasize, 0);
-    //printf("Bytes sent: %d\n", metadatasize);
 
     for(int i=0; i<message->size; ++i)
     {
@@ -44,7 +41,6 @@ int sendfile(char* IP, uint16_t port, char* pathtofile, int chunksize){
         int status = send(sock, (unsigned char *)message->packets[i].data, message->packets[i].size, 0);
         printf("status for %d: %d\n", i, status);
         read(sock, &buf, 1);
-        //printf("packet %d: %d bytes first char: %u last char: %u\n", i+1, message->packets[i].size, message->packets[i].data[0], message->packets[i].data[message->packets[i].size -1]);
     }
 
     // read the response
@@ -52,7 +48,6 @@ int sendfile(char* IP, uint16_t port, char* pathtofile, int chunksize){
     read(sock, resp, 1000);
     end = clock();
     cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    printf("Time for response: %f\n", cpu_time_used);
     printf("Response: \n%s\n", resp);
     close_connection(sock);
     freeMessage(message);
